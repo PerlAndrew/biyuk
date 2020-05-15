94e0eeecd201abfa46931cf8c50fabc29bd0c81d
PerlAndrew <61918684+PerlAndrew@users.noreply.github.com>
2020-05-15 23:02:44 +0300

    я есть сеньер

---
+++ alert.js
@@ -0,0 +1,16 @@
+/*const fun = setInterval(()=>{
+document.write('<p style="color:green,font-size:1500%">Вас взломали</p>'+ '</br>')
+},1000)*/
+/*const delay = (  wait = 1000) =>{
+const promise = new Promise((reselve,reject)=>{
+
+})
+setTimeout(()=>{resolve},wait)
+}*/
+const inp = document.getElementById('inp').value;
+const num1 = document.getElementById('number1')
+function go(){
+	if (num1.onclick){
+inp.value = "1";
+}}
+
